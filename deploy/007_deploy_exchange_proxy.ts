252fb3cf1e8ae6568f7ae06c1db7ce0c03da3166
vincentgeneste <vincentgeneste@gmail.com>
2023-01-18 14:38:57 -0800

    added script deploy lazy mint proxies

--- deploy/007_deploy_exchange_proxy.ts
+++ deploy/007_deploy_exchange_proxy.ts
@@ -1,7 +1,7 @@
 import {getSettings} from '../.config';
 import hre, {deployments, getNamedAccounts} from 'hardhat';
 import {ethers} from 'hardhat';
-import {COLLECTION, CRYPTO_PUNK} from '../test/utils/assets.js';
+import {ERC721_LAZY, ERC1155_LAZY, COLLECTION, CRYPTO_PUNK} from '../test/utils/assets.js';
 
 async function main() {
   const {deploy} = deployments;
@@ -11,6 +11,8 @@ async function main() {
   const FEES = getSettings(CHAIN).fees;
   const TRANSFER_PROXY = getSettings(CHAIN).transferProxy;
   const ERC20_TRANSFER_PROXY = getSettings(CHAIN).erc20TransferProxy;
+  const ERC721_LAZY_MINT_PROXY = getSettings(CHAIN).erc721LazyMintTransferProxy;
+  const ERC1155_LAZY_MINT_PROXY = getSettings(CHAIN).erc1155LazyMintTransferProxy;
   const ROYALTIES_PROXY = getSettings(CHAIN).royalties_proxy;
   const CUSTOM_MATCHER = getSettings(CHAIN).customMatcher;
   const PUNK_TRANSFER_PROXY = getSettings(CHAIN).punk_transfer_proxy;
@@ -37,6 +39,13 @@ async function main() {
   // add ExchangeV2 proxy address to the the allowed operators of transferProxy & erc20TransferProxy
   await TRANSFER_PROXY.addOperator(exchange_proxy.address);
   await ERC20_TRANSFER_PROXY.addOperator(exchange_proxy.address);
+  
+  // if required, set lazy mint proxies
+  if (ERC721_LAZY_MINT_PROXY && ERC1155_LAZY_MINT_PROXY) {
+    const exchangeV2Contract = await ethers.getContractAt('ExchangeV2', exchange_proxy.address);
+    await exchangeV2Contract.setTransferProxy(ERC721_LAZY, ERC721_LAZY_MINT_PROXY);
+    await exchangeV2Contract.setTransferProxy(ERC1155_LAZY, ERC1155_LAZY_MINT_PROXY);
+  }
 
   // if required, set collection asset matcher
   if (CUSTOM_MATCHER) {
