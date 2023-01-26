9b1ff31431571424a476ae5a23e770aedfeb4815
vincentgeneste <vincentgeneste@gmail.com>
2023-01-26 12:41:06 -0800

    added more UT for GhostMarket custom royalties

--- test/RoyaltiesERC1155.test.ts
+++ test/RoyaltiesERC1155.test.ts
@@ -1,25 +1,41 @@
 import {expect} from './utils/chai-setup';
-import {ethers} from 'hardhat';
-import {ERC1155WithRoyalties} from './../typechain';
+import {ethers, upgrades} from 'hardhat';
+import {ERC1155WithRoyalties, GhostMarketERC1155} from './../typechain';
 import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
 import {ZERO} from './utils/assets';
-import {INTERFACE_ID_ERC165, INTERFACE_ID_ROYALTIES_EIP2981} from './utils/constants';
+import {INTERFACE_ID_ERC165, INTERFACE_ID_GHOSTMARKET, INTERFACE_ID_ROYALTIES_EIP2981} from './utils/constants';
+import {BASE_URI, TOKEN_NAME, TOKEN_SYMBOL} from './utils/constants';
 
 describe('ERC1155WithRoyalties Test', () => {
   let deployer: SignerWithAddress;
   let randomAccount: SignerWithAddress;
   let royaltiesRecipient: SignerWithAddress;
   let erc1155WithRoyalties: ERC1155WithRoyalties;
+  let ghostMarketERC1155: GhostMarketERC1155;
 
   beforeEach(async () => {
     [deployer, randomAccount, royaltiesRecipient] = await ethers.getSigners();
     const ERC1155WithRoyalties = await ethers.getContractFactory('ERC1155WithRoyalties');
     erc1155WithRoyalties = await ERC1155WithRoyalties.deploy('ipfs://baseURI');
+    const GhostMarketERC1155 = await ethers.getContractFactory('GhostMarketERC1155');
+
+    ghostMarketERC1155 = <GhostMarketERC1155>await upgrades.deployProxy(
+      GhostMarketERC1155,
+      [TOKEN_NAME, TOKEN_SYMBOL, BASE_URI],
+      {
+        initializer: 'initialize',
+        unsafeAllowCustomTypes: true,
+      }
+    );
   });
 
   it('should have all the right interfaces', async function () {
     expect(await erc1155WithRoyalties.supportsInterface(INTERFACE_ID_ERC165), 'Error Royalties 165').to.be.true;
     expect(await erc1155WithRoyalties.supportsInterface(INTERFACE_ID_ROYALTIES_EIP2981), 'Error Royalties 2981').to.be
+      .true;
+
+    expect(await ghostMarketERC1155.supportsInterface(INTERFACE_ID_ERC165), 'Error Royalties 165').to.be.true;
+    expect(await ghostMarketERC1155.supportsInterface(INTERFACE_ID_GHOSTMARKET), 'Error Royalties Ghostmarket').to.be
       .true;
   });
 
@@ -32,6 +48,16 @@ describe('ERC1155WithRoyalties Test', () => {
       10001 // 100.01%
     );
     await expect(tx).to.be.revertedWith('ERC2981Royalties: Too high');
+
+    const tx2 = ghostMarketERC1155.mintGhost(
+      deployer.address,
+      1,
+      '0x',
+      [{recipient: deployer.address, value: 5001}],
+      'ext_uri',
+      ''
+    );
+    await expect(tx2).to.be.revertedWith('Royalty total value should be < 50%');
   });
 
   it('should have the right royalties for tokenId', async function () {
@@ -45,6 +71,18 @@ describe('ERC1155WithRoyalties Test', () => {
     const info = await erc1155WithRoyalties.royaltyInfo(0, 100);
     expect(info[1].toNumber()).to.be.equal(10);
     expect(info[0]).to.be.equal(royaltiesRecipient.address);
+
+    await ghostMarketERC1155.mintGhost(
+      deployer.address,
+      1,
+      '0x',
+      [{recipient: royaltiesRecipient.address, value: 250}],
+      'ext_uri',
+      ''
+    );
+    const info2 = await ghostMarketERC1155.getRoyalties(1);
+    expect(info2[0][0]).to.be.equal(royaltiesRecipient.address);
+    expect(info2[0][1]).to.be.equal(250);
   });
 
   it('should be able to set address(0) as royalties recipient', async function () {
@@ -52,6 +90,9 @@ describe('ERC1155WithRoyalties Test', () => {
     const info = await erc1155WithRoyalties.royaltyInfo(0, 100);
     expect(info[1].toNumber()).to.be.equal(10);
     expect(info[0]).to.be.equal(ZERO);
+
+    const tx = ghostMarketERC1155.mintGhost(deployer.address, 1, '0x', [{recipient: ZERO, value: 250}], 'ext_uri', '');
+    await expect(tx).to.be.revertedWith('Recipient should be present');
   });
 
   it('should have no royalties if not set', async function () {
@@ -59,6 +100,10 @@ describe('ERC1155WithRoyalties Test', () => {
     const info = await erc1155WithRoyalties.royaltyInfo(0, 100);
     expect(info[1].toNumber()).to.be.equal(0);
     expect(info[0]).to.be.equal(ZERO);
+
+    await ghostMarketERC1155.mintGhost(deployer.address, 1, '0x', [], 'ext_uri', '');
+    const info2 = await ghostMarketERC1155.getRoyalties(1);
+    expect(info2.length).to.be.equal(0);
   });
 
   it('should have each token with the right royalties when minting batch', async function () {
--- test/RoyaltiesERC721.test.ts
+++ test/RoyaltiesERC721.test.ts
@@ -1,19 +1,36 @@
 import {expect} from './utils/chai-setup';
-import {ethers} from 'hardhat';
-import {ERC721WithRoyalties} from '../typechain';
+import {ethers, upgrades} from 'hardhat';
+import {ERC721WithRoyalties, GhostMarketERC721} from '../typechain';
 import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
 import {ZERO} from './utils/assets';
-import {INTERFACE_ID_ERC165, INTERFACE_ID_ERC721, INTERFACE_ID_ROYALTIES_EIP2981} from './utils/constants';
+import {
+  INTERFACE_ID_ERC165,
+  INTERFACE_ID_ERC721,
+  INTERFACE_ID_GHOSTMARKET,
+  INTERFACE_ID_ROYALTIES_EIP2981,
+} from './utils/constants';
+import {BASE_URI, TOKEN_NAME, TOKEN_SYMBOL} from './utils/constants';
 
 describe('ERC721WithRoyalties Test', () => {
   let deployer: SignerWithAddress;
   let royaltiesRecipient: SignerWithAddress;
   let erc721WithRoyalties: ERC721WithRoyalties;
+  let ghostMarketERC721: GhostMarketERC721;
 
   beforeEach(async () => {
     [deployer, royaltiesRecipient] = await ethers.getSigners();
     const ERC721WithRoyalties = await ethers.getContractFactory('ERC721WithRoyalties');
     erc721WithRoyalties = await ERC721WithRoyalties.deploy('ERC721WithRoyalties', '2981');
+    const GhostMarketERC721 = await ethers.getContractFactory('GhostMarketERC721');
+
+    ghostMarketERC721 = <GhostMarketERC721>await upgrades.deployProxy(
+      GhostMarketERC721,
+      [TOKEN_NAME, TOKEN_SYMBOL, BASE_URI],
+      {
+        initializer: 'initialize',
+        unsafeAllowCustomTypes: true,
+      }
+    );
   });
 
   it('should have all the right interfaces', async function () {
@@ -21,6 +38,11 @@ describe('ERC721WithRoyalties Test', () => {
     expect(await erc721WithRoyalties.supportsInterface(INTERFACE_ID_ROYALTIES_EIP2981), 'Error Royalties 2981').to.be
       .true;
     expect(await erc721WithRoyalties.supportsInterface(INTERFACE_ID_ERC721), 'Error Royalties 721').to.be.true;
+
+    expect(await ghostMarketERC721.supportsInterface(INTERFACE_ID_ERC165), 'Error Royalties 165').to.be.true;
+    expect(await ghostMarketERC721.supportsInterface(INTERFACE_ID_ERC721), 'Error Royalties 721').to.be.true;
+    expect(await ghostMarketERC721.supportsInterface(INTERFACE_ID_GHOSTMARKET), 'Error Royalties Ghostmarket').to.be
+      .true;
   });
 
   it('should throw if royalties more than 100%', async function () {
@@ -30,6 +52,14 @@ describe('ERC721WithRoyalties Test', () => {
       10001 // 100.01%
     );
     await expect(tx).to.be.revertedWith('ERC2981Royalties: Too high');
+
+    const tx2 = ghostMarketERC721.mintGhost(
+      deployer.address,
+      [{recipient: deployer.address, value: 5001}],
+      'ext_uri',
+      ''
+    );
+    await expect(tx2).to.be.revertedWith('Royalty total value should be < 50%');
   });
 
   it('should have the right royalties for tokenId', async function () {
@@ -41,6 +71,16 @@ describe('ERC721WithRoyalties Test', () => {
     const info = await erc721WithRoyalties.royaltyInfo(1, 10000);
     expect(info[1].toNumber()).to.be.equal(250);
     expect(info[0]).to.be.equal(royaltiesRecipient.address);
+
+    await ghostMarketERC721.mintGhost(
+      deployer.address,
+      [{recipient: royaltiesRecipient.address, value: 250}],
+      'ext_uri',
+      ''
+    );
+    const info2 = await ghostMarketERC721.getRoyalties(1);
+    expect(info2[0][0]).to.be.equal(royaltiesRecipient.address);
+    expect(info2[0][1]).to.be.equal(250);
   });
 
   it('should be able to set address(0) as royalties recipient', async function () {
@@ -49,6 +89,9 @@ describe('ERC721WithRoyalties Test', () => {
     const info = await erc721WithRoyalties.royaltyInfo(1, 10000);
     expect(info[1].toNumber()).to.be.equal(1);
     expect(info[0]).to.be.equal(ZERO);
+
+    const tx = ghostMarketERC721.mintGhost(deployer.address, [{recipient: ZERO, value: 250}], 'ext_uri', '');
+    await expect(tx).to.be.revertedWith('Recipient should be present');
   });
 
   it('should have no royalties if not set', async function () {
@@ -56,6 +99,10 @@ describe('ERC721WithRoyalties Test', () => {
     const info = await erc721WithRoyalties.royaltyInfo(1, 100);
     expect(info[1].toNumber()).to.be.equal(0);
     expect(info[0]).to.be.equal(ZERO);
+
+    await ghostMarketERC721.mintGhost(deployer.address, [], 'ext_uri', '');
+    const info2 = await ghostMarketERC721.getRoyalties(1);
+    expect(info2.length).to.be.equal(0);
   });
 
   it('should have each token with the right royalties when minting batch', async function () {
